a9a06c0e6191b337e2c334be120073d7464291e9
Fares Essayeh <forusessayeh@gmail.com>
2022-08-08 11:28:32 +0200

    borders and friends

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Karim from './components/Karim'
+import Fares from './components/Fares';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Karim/>
-        <div className='circle'>
-
-</div>
-      </header>
+      <Fares />
     </div>
   );
 }
---
+++ src/components/Fares.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Fares = () => {
+   return (
+      <>
+         <div className="fares">Background</div>
+         <div className="fares-border">no background</div>
+      </>
+   )
+}
+
+export default Fares
